a5672627a6264a799541f71b81680ff50a73c9c9
shaffin <i212963@nu.edu.pk>
2025-04-20 14:33:46 +0500

    API Dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,11 @@ dependencies {
     implementation ("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation( "androidx.room:room-ktx:2.6.1")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     implementation(libs.core.ktx)
     implementation(libs.androidx.junit.ktx)
     implementation(libs.androidx.espresso.intents)
