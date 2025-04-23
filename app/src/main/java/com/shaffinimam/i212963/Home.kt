6bfe744847c720bd5f4249b1dcd64917d68943b5
shaffin <i212963@nu.edu.pk>
2025-04-23 22:19:49 +0500

    Post Completed

--- app/src/main/java/com/shaffinimam/i212963/Home.kt
+++ app/src/main/java/com/shaffinimam/i212963/Home.kt
@@ -1,6 +1,7 @@
 package com.shaffinimam.i212963
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Base64
@@ -8,6 +9,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -61,7 +63,11 @@ class Home : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val dm = view.findViewById<ImageButton>(R.id.dm);
+        dm.setOnClickListener {
+            val intent = Intent(requireContext(), DM::class.java)
+            startActivity(intent)
+        }
         rvStories = view.findViewById(R.id.rvStories)
         rvStories.layoutManager = LinearLayoutManager(
             requireContext(),
